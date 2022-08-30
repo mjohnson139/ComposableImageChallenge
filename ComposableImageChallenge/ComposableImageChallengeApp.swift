7da8d1bb08b2df4397d36ded15537781250d9f64
Matt Johnson <matt@roofsnap.com>
2022-08-30 08:19:08 -0400

    Initial Commit

---
+++ ComposableImageChallenge/ComposableImageChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableImageChallengeApp.swift
+//  ComposableImageChallenge
+//
+//  Created by Matt Johnson on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct ComposableImageChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
