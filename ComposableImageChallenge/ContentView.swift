49e29e26e154e042c55b8335d289e7e0695ec486
Matt Johnson <matt@roofsnap.com>
2022-08-30 11:56:18 -0400

    Matt’s first solution.

--- ComposableImageChallenge/ContentView.swift
+++ ComposableImageChallenge/ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ComposableImage(url: URL(string: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvariety.com%2Fwp-content%2Fuploads%2F2021%2F07%2FRick-Astley-Never-Gonna-Give-You-Up.png%3Fw%3D681%26h%3D383%26crop%3D1&imgrefurl=https%3A%2F%2Fvariety.com%2F2021%2Fdigital%2Fnews%2Frick-astley-never-gonna-give-you-up-1-billion-youtube-views-1235030404%2F&tbnid=mpEY6_gX6pGR_M&vet=12ahUKEwi878WUx-75AhWVn3IEHVByCu4QMygCegUIARDnAQ..i&docid=0tR9HAdgSrzjFM&w=681&h=383&q=rick%20astley&ved=2ahUKEwi878WUx-75AhWVn3IEHVByCu4QMygCegUIARDnAQ"))
+        ComposableImage(url: URL(string: "https://cdn.vox-cdn.com/thumbor/HWPOwK-35K4Zkh3_t5Djz8od-jE=/0x86:1192x710/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg"))
             { $0 }
         placeHolder: {
             Image(systemName: "photo")
--- ComposableImageChallenge/TheChallenge.swift
+++ ComposableImageChallenge/TheChallenge.swift
@@ -1,31 +1,76 @@
+import Combine
 import SwiftUI
 /**
  The goal is to make a “composable” image, that basically acts just like AsyncImage and doesn’t have the iOS 15 constraint.
- 
+
  Implement the following  data type.
- 
+
  This type is initialized with
     1) an optional URL,
     2) a function that takes and image, and builds a view using that image
     3) a function that builds a placeholder view for when the image is not present.
- 
+
  The initializer in The Composable architecture for the IfLetStore is very similar to what we want here, in that
  we need a ViewBuilder that builds two different pieces of content, depending on the state of the image that is fetched from the url.
- 
+
  The solution should have use combine to publish the image when it becomes available.
- 
- This message will self destruct in 10 seconds. 
- 
+
+ This message will self destruct in 10 seconds.
+
  */
 
 struct ComposableImage<Content>: View where Content: View {
+    @ObservedObject private var model: ComposableImage.Model
+    private var contentBuilder: (Image?) -> Content
+    private var cancellables = Set<AnyCancellable>()
+
     public init<I, P>(
         url: URL?,
         @ViewBuilder content: @escaping (Image) -> I,
         @ViewBuilder placeHolder: @escaping () -> P
-    ) where Content == _ConditionalContent<I, P>, I: View, P: View {}
+    ) where Content == _ConditionalContent<I, P>, I: View, P: View {
+        print(url ?? "no url")
+        model = Model(url: url)
+        contentBuilder = { image in
+            if let image = image {
+                return ViewBuilder.buildEither(first: content(image))
+            } else {
+                return ViewBuilder.buildEither(second: placeHolder())
+            }
+        }
+    }
 
     var body: some View {
-        Text("Unimplemented")
+        contentBuilder(self.model.image)
+    }
+}
+
+private extension ComposableImage {
+    class Model: ObservableObject {
+        @Published private(set) var image: Image? = nil
+
+        init(url: URL?) {
+            imageSubscription(url: url)
+        }
+
+        func imageSubscription(url: URL?) {
+            Just(url)
+                .compactMap { $0 }
+                .flatMap { url in
+                    URLSession.shared
+                        .dataTaskPublisher(for: url)
+                }
+                .map(\.data)
+                .tryMap { data in
+                    UIImage(data: data)
+                }
+                .compactMap { $0 }
+                .map(Image.init(uiImage:))
+                .catch { _ in Just(nil) }
+                .eraseToAnyPublisher()
+                .print()
+                .assign(to: &$image)
+                
+        }
     }
 }
